8a05873f850ea22772cafb2bd74ac29084b5d4b3
xiang <xiang@JASPER-SURFACE>
2017-05-26 11:20:51 +1000

    Add project files.

---
+++ WebApplication1/Controllers/AuthenticationController.cs
@@ -0,0 +1,58 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        // GET: Authentication
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(String ReturnUrl)
+        {
+            ViewData["ReturnUrl"] = ReturnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DoLogin(LoginViewModelcs loginViewModel, String BtnSubmit, String returnURL)
+        {
+            if (loginViewModel.Username == "admin" && loginViewModel.Password == "admin")
+            {
+                //String baseHost = Request.Url.GetLeftPart(UriPartial.Authority) + Url.Content("~");
+                //String host = baseHost.Remove(baseHost.Length - 1);
+                //String request = host + returnURL;
+                //return Redirect(request);
+                FormsAuthentication.SetAuthCookie(loginViewModel.Username, false);
+                return Redirect(returnURL);
+            }
+            else
+            {
+                ModelState.AddModelError("CredentialError", "Invalid Username or Password");
+                return View("Login");
+            }
+            return View("Login");
+        }
+
+        public ActionResult Test()
+        {
+            LoginViewModelcs loginViewModel = new LoginViewModelcs() { Username = "", Password = "Password" };
+            return View(loginViewModel);
+        }
+
+        public ActionResult JJPartial()
+        {
+            return PartialView("JJPartialView");
+        }
+
+    }
+}
---
+++ WebApplication1/Models/LoginViewModelcs.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    [DisplayName("LoginViewModelCs")]
+    public class LoginViewModelcs
+    {
+        String username;
+        String password;
+
+        public LoginViewModelcs(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        public LoginViewModelcs()
+        {
+        }
+
+        [DisplayName("JJ Username Display")]
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+
+            set
+            {
+                username = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
+
+    }
+}
